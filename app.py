c16b78f43f91b4e923b4af971e67519e57aa1160
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-14 14:49:52 -0400

    added pages

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('homepage.html')
 
+@app.route('/shows')
+def shows():
+    return render_template('shows.html')
+
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
